9f9695c619c6ed2b02fd8554bff6a17d8b5ec4ff
marquez22 <49456890+marquez22@users.noreply.github.com>
2019-04-12 08:21:12 -0600

    This is the start of doctor class.

---
+++ doctor.hpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <queue>
+#include "patients.hpp"
+
+class Doctor
+{
+  private:
+  std::string specialization;
+  queue<Patients>;
+
+  public:
+  Doctor(std::string specialization);
+
+}
