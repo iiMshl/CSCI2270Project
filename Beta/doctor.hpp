1424bf16b329983e9c4c0a9256c78cf814ee4ce0
marquez22 <49456890+marquez22@users.noreply.github.com>
2019-04-12 08:09:59 -0600

    This is the start of the doctors class.

---
+++ Beta/doctor.hpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <queue>
+#include "patients.hpp"
+
+class Doctor
+{
+  private:
+  string specialization;
+  queue<Patients>;
+
+  public:
+  Doctor(string specialization);
+  
+}
