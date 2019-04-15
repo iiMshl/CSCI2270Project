4bdff13511f2da8065f79235f688fe2685c2feb8
marquez22 <49456890+marquez22@users.noreply.github.com>
2019-04-15 13:21:56 -0600

    Update doctor.hpp

--- doctor.hpp
+++ doctor.hpp
@@ -7,6 +7,7 @@ class Doctor
   private:
   std::string specialization;
   queue<Patients>;
+  vector<int>; // This vector will contain the days of the month for which the doctor has available appointments
 
   public:
   Doctor(std::string specialization);
