6886a728aa48856bf214717f1ec9d0cdf7224d9c
Meshal <imeshalfm@icloud.com>
2019-04-15 20:08:47 -0600

    reuploaded Driver class

---
+++ Beta/Driver.hpp
@@ -0,0 +1,52 @@
+
+#ifndef Driver_hpp
+#define Driver_hpp
+
+#include <string>
+#include <cstring>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+#include "PriorityQueue.hpp"
+
+using namespace std;
+
+
+
+struct doctorNode {
+    
+    int cat;
+    vector<int> availability;
+    PriorityQueue pq();
+    
+    struct doctorNode* next; // pointer to the next node
+    
+};
+
+
+class Driver{
+    
+public:
+    
+    Driver();
+    ~Driver();
+    
+    
+    void setDoctors();
+    
+    
+    void assignPatient(string name,int esi,int cat);
+    void removePatient(string name,int cat);
+    
+    
+    
+private:
+    
+    vector<doctorNode> doctors;
+    vector<patientNode> patients;
+    
+};
+
+
+#endif /* Driver_hpp */
