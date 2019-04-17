d316292d3b6fe1c5c6d9e79f04e386cec84cd4b3
marquez22 <49456890+marquez22@users.noreply.github.com>
2019-04-16 21:44:49 -0600

    This is the implementation of the calendar
    We can add more features later on, this is jut the basics to schedule an appointment.

---
+++ hashCalendar.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include<iostream>
+//#include <map>
+ struct CalendarNode
+ {
+   int day;
+   std::string doctorName;
+   bool firstApp;
+   bool secondApp;
+   std::string Patient1;
+   std::string Patient2;
+   CalendarNode* next;
+   CalendarNode(std::string _doctorName, int _day)
+   {
+     doctorName = _doctorName;
+     day = _day;
+     firstApp = false;
+     secondApp = false;
+     Patient1 = "";
+     Patient2 = "";
+   }
+ };
+
+class Calendar
+{
+  private:
+  int getHash(int _day);
+  CalendarNode* searchTable(int _day,std::string word);
+
+  /* instance variables */
+  CalendarNode** hashCalendar;
+  int size;
+  public:
+  Calendar();
+  void addDoctorDay(int _day, std::string _doctorName);
+  int checkDay(std::string _doctorName,int _day);
+  void viewDoctorAvailavility(std::string _doctorName);
+  void bookAppointment(std::string _doctorName, int day,std::string _patient);
+  void cancelAppointment(std::string _doctorName, int _day, std::string _patient);
+};
