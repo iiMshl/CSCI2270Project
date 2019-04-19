961ed361b5c57dfab2d6d32d7c7252c457d14364
Meshal <imeshalfm@icloud.com>
2019-04-19 12:05:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,121 +1,181 @@
-#include<iostream>
-#include<string>
-#include<sstream>
-#include<fstream>
-#include<map>
-#include<vector>
+
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "PriorityQueue.hpp"
+#include "Driver.hpp"
 #include "hashCalendar.hpp"
-#include "doctor.hpp"
-// #include "patient.hpp"
-void readDoctors(std::string filename,)
-{
-  std::ifstream fin(filename);
-  if(fin.is_open())
-  {
-    // User usersIF[100]; // Address database (array of structures)
-    // int size = 0;
-    std::string line;
-    while(getline(fin,line))
-    {
-      std::string _specialization, _days, _doctorName;// create temporary holders to later convert to floats and integers.
-     int days;
-      std::string d;
-      std::vector<int> addDays;
-    //   float gpa;
-    //   float mingpa = atof(argv[3]);
-      std::stringstream sin(line);
-      //std::stringstream din(_days);
-      getline(sin, _doctorName, ',');
-      getline(sin, _specialization, ',');
-      getline(sin, _days, ',');
-      std::cout << _doctorName << " " << _specialization <<  std::endl;//" " << _days << std::endl;
-      std::stringstream tin(_days);
-      while(getline(tin,d,' '))
-      {
-        days = stoi(d);
-        std::cout << days  << " - ";// add the days to vector
-      }
-    //   age = stoi(tempage);
-    //   gpa = stof(tempgpa);
-    //   if(mingpa <= gpa)
-    //   {
-    //     oFile << line << std::endl;
-    //   }
-    //   addUser(usersIF, name, gpa, age,size);
 
-    //   size++;
+using namespace std;
 
-    std::cout << std::endl;
+
+
+int getESI(){
+    
+    string choice;
+    bool invalid = true;
+    
+    while (invalid) {
+        invalid = false;
+        
+        cout << "does it require immediate life-saving intervention? \n(1.) Yes (2.) No" << endl;
+        getline(cin,choice);
+        
+        //Q1 Answer
+        if (choice=="1") {
+            return 1;
+            
+        } else if (choice=="2"){
+            
+            cout << "Is it a high risk situation, or a severe pain/distress? \n(1.) Yes (2.) No" << endl;
+            getline(cin,choice);
+            
+            //Q2 Answer
+            if (choice=="1") {
+                return 2;
+            } else if (choice=="2"){
+                
+                cout << "how many different resources are needed for it? \n(1.) None (2.) One (3.) Many" << endl;
+                getline(cin,choice);
+                
+                //Q3 Answer
+                if (choice=="1") {
+                    return 5;
+                } else if (choice=="2"){
+                    return 4;
+                } else if (choice=="3"){
+                    
+                    cout << "are vitals in danger zone? \n(1.) Yes (2.) No" << endl;
+                    getline(cin,choice);
+                    
+                    //Q4 Answer
+                    if (choice=="1") {
+                        return 2;
+                    } else if (choice=="2"){
+                        return 3;
+                    } else { cout<<"Invalid input"<<endl; invalid=true;continue;}
+                    
+                } else { cout<<"Invalid input"<<endl; invalid=true;continue;}
+
+            } else { cout<<"Invalid input"<<endl; invalid=true;continue;}
+
+        } else { cout<<"Invalid input"<<endl; invalid=true;continue;}
+
     }
-    // std::cout << calendar.size() << " size of table";
-  }
-    fin.close();
+    
+    return 5;
+}
+
+void showCats(){
+    
+    cout << "1. Heart/Blood Vessels (Cardiologist)" << endl;
+    cout << "2. " << endl;
+    cout << "3. " << endl;
+    cout << "4. " << endl;
+    cout << "5. " << endl;
+    cout << "6. " << endl;
+    cout << "7. " << endl;
+    cout << "8. " << endl;
+    cout << "9. Other (General)" << endl;
+    
+}
+
+void menu(){
+    cout << "============Main Menu============" << endl;
+    cout << "1. Get patients information from file" << endl;
+    cout << "2. Add a patient" << endl;
+    cout << "3. Show next patients in the queue from all sections" << endl;
+    cout << "4. Serve next patient" << endl;
+    cout << "5. Clear Entire Section" << endl;
+    cout << "6. Quit" << endl;
 }
 
 
-int main()
-{
-  Calendar m;
-  doctorHash d;
-  // std::vector<int> myVec;
-  //std::map<int,std::string> ma;
-  std::string word = "1234";
-  std::vector<int> v;
-  int sum = 0;
-  for(int i = 0;i<word.length();i++)
-  {
-    sum = sum + word[i];
-    v.push_back(sum);
-    std::cout << sum <<  " main  "  << std::endl;
-  }
-  d.addDoctor("cardiologist","Erik",v,true,"Lupita",m);
-  // std::vector<int> t;
-  // // v = t;
-  // for(int j = 0; j < v.size() ;j++)
-  // {
-  //   t.push_back(v[j]);
-  //   std::cout << v[j]  << " " << t[j] <<std::endl;
-  // }
-  //
-  // for(int i = 0; i < 5; i++)
-  // {
-  //   myVec.push_back(i);
-  // }
-  // ma.insert( std::pair<int,std::string>(6,"erik"));
-  // ma.insert( std::pair<int,std::string>(6,"erik"));
-  // std::cout << ma.size() << std::endl;
-  // ma.insert( std::pair<int,std::string>(4,"alan"));
-  // std::cout << ma.size() << std::endl;
-  // std::ifstream fin("doctors.txt");
-  // if(fin.is_open())
-  // {
-  //   // User usersIF[100]; // Address database (array of structures)
-  //   // int size = 0;
-  //   std::string line;
-  //   while(getline(fin,line))
-  //   // {
-  //   //   std::string name, tempgpa, tempage; // create temporary holders to later convert to floats and integers.
-  //   //   int age;
-  //   //   float gpa;
-  //   //   float mingpa = atof(argv[3]);
-  //   //   std::stringstream sin(line);
-  //   //   getline(sin, name, ',');
-  //   //   getline(sin, tempgpa, ',');
-  //   //   getline(sin, tempage, ' ');
-  //   //   age = stoi(tempage);
-  //   //   gpa = stof(tempgpa);
-  //   //   if(mingpa <= gpa)
-  //   //   {
-  //   //     oFile << line << std::endl;
-  //   //   }
-  //   //   addUser(usersIF, name, gpa, age,size);
-  //
-  //   //   size++;
-  //
-  //   std::cout << line << std::endl;
-  //   }
-  // //}
-  //   fin.close();
-  // readDoctors("doctors.txt");
-  return 0;
+int main(int argc, char const *argv[]){
+    
+    string choice;
+    
+    Driver dr;
+    
+    dr.setDoctors("doctors.txt");
+    
+    while (choice != "6") {
+        menu();
+        getline(cin, choice);
+        switch (stoi(choice)) {
+            case 1:{
+                
+                
+                
+                
+                break;
+            }
+            case 2:{
+                string name;
+                int esi;
+                string cat;
+                string age;
+                
+                cout << "Enter Patient Name: " << endl;
+                getline(cin,name);
+                
+                cout << "Enter Patient Age: " << endl;
+                getline(cin,age);
+                
+                cout << "What Would your injury categorize as?" << endl;
+                showCats();
+                getline(cin,cat);
+                
+                esi = getESI();
+                
+                dr.assignPatient(name, esi, stoi(cat),stoi(age));
+                
+                
+                break;
+            }
+            case 3:{
+                
+                dr.showNextPatient();
+                
+                break;
+            }
+            case 4:{
+                
+                string entry;
+                
+                cout << "Choose A Category (1-9) Enter 0 To Show Categories" << endl;
+                getline(cin,entry);
+                
+                while (entry < "1") {
+                    if (entry == "0") {
+                        showCats();
+                    }
+                    cout << "Choose A Category (1-9) Enter 0 To Show Categories" << endl;
+                    getline(cin,entry);
+                }
+                
+                dr.removePatient(stoi(entry));
+                
+                break;
+            }
+            case 5:{
+                
+                
+                
+                break;
+            }
+            case 6:{
+                
+                cout << "Goodbye!" << endl;
+                
+                break;
+            }
+            default:
+                cout << "invalid input" << endl << endl;
+        }
+    }
+    
 }
