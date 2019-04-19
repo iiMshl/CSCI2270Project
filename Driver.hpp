2e6c18752fff6224b675324ddd82b6fd0cf15c97
Meshal <imeshalfm@icloud.com>
2019-04-19 09:34:29 -0600

    Update Driver

---
+++ Driver.hpp
@@ -0,0 +1,71 @@
+
+#ifndef Driver_hpp
+#define Driver_hpp
+
+#include <string>
+#include <cstring>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+#include "PriorityQueue.hpp"
+#include "hashCalendar.hpp"
+
+using namespace std;
+
+
+
+struct doctorsVect {
+    
+    string name;
+    
+    vector<int> availability;
+    string profession;
+    bool followup;
+    string fDoctor;
+    
+    PriorityQueue patients();
+    //Calendar cal;
+    
+    
+    
+    doctorsVect(string _name,vector<int> _availability,string _profession,bool _followup,string _fDoctor){
+        
+        name = _name;
+        availability = _availability;
+        profession = _profession;
+        followup = _followup;
+        fDoctor = _fDoctor;
+        
+        
+    }
+    
+};
+
+
+class Driver{
+    
+public:
+    
+    Driver();
+    ~Driver();
+    
+    
+    void setDoctors(string);
+    
+    
+    void assignPatient(string name,int esi,int cat);
+    void removePatient(int cat);
+    void showNextPatient();
+    
+    
+private:
+    
+    vector<doctorsVect> doctors;
+    Calendar calendar;
+    //vector<patientNode> patients;
+    
+};
+
+
+#endif /* Driver_hpp */
